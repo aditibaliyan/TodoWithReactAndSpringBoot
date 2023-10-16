d16b869d085f937119acc488787c92441c4dba77
Dell <Dell@DESKTOP-L2TQJ9S>
2023-10-16 12:01:21 +0530

    initial commit.

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Todo (id, username, description, target_date, done) values ('10001', 'in28minutes','AWS 1', current_date(), false)
+insert into Todo (id, username, description, target_date, done) values ('10002', 'in28minutes','AWS 2', current_date(), false)
+insert into Todo (id, username, description, target_date, done) values ('10003', 'in28minutes','AWS 3', current_date(), false)
